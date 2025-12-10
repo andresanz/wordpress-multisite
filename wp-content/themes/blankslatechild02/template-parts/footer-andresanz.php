1fcab9991fd889f2a3f906763b3fbf263aeee655
Andre Sanz <sanz.andre@gmail.com>
2025-12-10 03:00:04 -0500

    Automated commit on 2025-12-10 03:00:01

--- wp-content/themes/blankslatechild02/template-parts/footer-andresanz.php
+++ wp-content/themes/blankslatechild02/template-parts/footer-andresanz.php
@@ -2,13 +2,7 @@
   <footer>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
 
-    <div class="social-icons">
-      <a href="https://www.linkedin.com/in/andresanz/" target="_blank" rel="noopener" aria-label="Linkedin">
-        <i class="fa-brands fa-linkedin"></i>
-      </a>
-
-    </div>
-
+    
   </footer>
 
 
