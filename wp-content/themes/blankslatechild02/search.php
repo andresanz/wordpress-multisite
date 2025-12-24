dfe383f38dd773697d8236484de4cc3dee1180c2
Andre Sanz <sanz.andre@gmail.com>
2025-12-24 15:00:06 -0500

    Automated commit on 2025-12-24 15:00:01

--- wp-content/themes/blankslatechild02/search.php
+++ wp-content/themes/blankslatechild02/search.php
@@ -11,13 +11,25 @@
                 } elseif (is_tag()) {
                     echo 'Tag: ' . single_tag_title('', false);
                 } elseif (is_search()) {
-                    echo 'Search Results for: ' . get_search_query();
+                    if (have_posts()) {
+                        echo 'Search Results for: ' . get_search_query();
+                    } else {
+                        echo 'No results found for: ' . get_search_query();
+                    }
                 } else {
                     the_archive_title();
                 }
                 ?>
             </h1>
-            <?php the_archive_description('<div class="archive-description">', '</div>'); ?>
+            <?php if (is_search() && !have_posts()) : ?>
+                <div class="archive-description no-results">
+                    <p>Nothing matched your search. Try a different keyword.</p>
+                    <?php get_search_form(); ?>
+                </div>
+            <?php endif; ?>
+            <?php if (!is_search()) : ?>
+                <?php the_archive_description('<div class="archive-description">', '</div>'); ?>
+            <?php endif; ?>
         </header>
 
         <section class="posts">
@@ -29,7 +41,9 @@
                 <?php the_posts_pagination(); ?>
 
             <?php else : ?>
-                <?php get_template_part('template-parts/content', 'none'); ?>
+                <?php if (!is_search()) : ?>
+                    <?php get_template_part('template-parts/content', 'none'); ?>
+                <?php endif; ?>
             <?php endif; ?>
         </section>
 
