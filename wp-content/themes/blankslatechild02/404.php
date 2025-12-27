5b4c42513ac8a5ef06827f2f849260d5622e37ef
Andre Sanz <sanz.andre@gmail.com>
2025-12-27 03:00:11 -0500

    Automated commit on 2025-12-27 03:00:01

--- wp-content/themes/blankslatechild02/404.php
+++ wp-content/themes/blankslatechild02/404.php
@@ -2,16 +2,18 @@
 get_header();
 ?>
 
-<main id="primary" class="site-main rc-404">
+<main id="primary" class="site-main rc-404 roundedrectangle">
   <section class="rc-404__wrap">
     <h1 class="rc-404__title">Nope.</h1>
     <p class="rc-404__text">
       This page doesn’t exist. Either the link is wrong or you moved it and forgot to tell the internet.
     </p>
 
+    <!--
     <div class="rc-404__actions">
       <a class="rc-btn" href="<?php echo esc_url(home_url('/')); ?>">Go home</a>
     </div>
+-->
 
     <div class="rc-404__search">
       <?php get_search_form(); ?>
